75881d419be4ede4171cebf85fdd9c8a42b9e85c
ilakonovalov <antonpuckich@gmail.com>
2024-04-20 21:01:12 +0300

    Add skeleton for client and map parser commands to corresponding functions

---
+++ client/src/client.py
@@ -0,0 +1,22 @@
+def init(args):
+    pass
+
+
+def status(args):
+    pass
+
+
+def add(args):
+    pass
+
+
+def commit(args):
+    pass
+
+
+def reset(args):
+    pass
+
+
+def log(args):
+    pass
--- client/src/main.py
+++ client/src/main.py
@@ -1,22 +1,32 @@
 import argparse
-if __name__ == '__main__':
 
+from client import *
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='VCS')
     subparsers = parser.add_subparsers()
 
-    subparsers.add_parser('init', help='Init repo in current directory')
-    subparsers.add_parser('status', help='Show file statuses')
-    subparsers.add_parser('log', help='Show commits log')
+    init_parser = subparsers.add_parser('init', help='Init repo in current directory')
+    init_parser.set_defaults(func=init)
+
+    status_parser = subparsers.add_parser('status', help='Show file statuses')
+    status_parser.set_defaults(func=status)
+
+    log_parser = subparsers.add_parser('log', help='Show commits log')
+    log_parser.set_defaults(func=log)
 
     add_parser = subparsers.add_parser('add', help='Start tracking file')
-    add_parser.add_argument('files', nargs='+', help='List of files to add')
+    add_parser.add_argument('files', nargs='*',
+                            help='List of files to add. If no files specified add all from root directory.')
+    add_parser.set_defaults(func=add)
 
     commit_parser = subparsers.add_parser('commit', help='Commit files (send to the server)')
     commit_parser.add_argument('message', help='Commit message')
+    commit_parser.set_defaults(func=commit)
 
     reset_parser = subparsers.add_parser('reset', help='Reset repository state to specified commit')
     reset_parser.add_argument('hash', help='Commit hash')
+    reset_parser.set_defaults(func=reset)
 
     args = parser.parse_args()
-
-    print(args)
+    args.func(args)
