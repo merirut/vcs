959507f5331665636358a51d8cdce63e1f713c5e
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-04-20 14:51:00 +0300

    add some realization to vsc.c, still isn't even remotely finished

--- client/vcs.c
+++ client/vcs.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <dirent.h>
+
+void recursive_check_if_changed (const char *dir_path) {
+	DIR *dir = opendir(dir_path);
+	if (dir == NULL) {
+		printf("Error opening directory");
+	}
+	struct dirent *entry;
+	while ((entry = readdir(dir)) != NULL) {
+		if (!strcmp(entry -> d_name, ".") && !strcmp(entry -> d_name, "..")) {
+			if (entry -> d_type == DT_DIR && !strcmp(entry->d_name, ".vcs")) {
+				recursive_check_if_changed();
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
 	if (argc == 1) {
@@ -6,16 +23,58 @@ int main(int argc, char *argv[]) {
 		return 0;
 	} else if (argc == 2) {
 		char* command = argv[1];
-		if (strcmp(command, "init")) {
-			printf("Initialization was successful");
-		} else if (strcmp(command, "status")) {
-			printf("Displaying status");
-		} else if(strcmp(command, "log")) {
-			printf("Displaying log");
-		} else if(strcmp(command, "add")) {
-			printf("Adding something");
-		} else if(strcmp(command, "reset")) {
-			printf("Reverting changes");
+		char* absolute_root_path = argv[2];
+		DIR* dir = opendir(argv[2]);
+		if (dir == NULL) {
+			printf("Error opening directory");
+			return 1;
 		}
+		if (strcmp(command, "init")) {
+			printf("Initialization was successful\n");
+			return 0;
+		} else {
+			struct dirent *entry;
+			int vcs_folder_found = 0;
+			while ((entry = readdir(dir)) != NULL) {
+				if (entry -> d_type == DT_DIR && strcmp(entry->d_name, ".vcs") == 0) {
+					vcs_folder_found = 1;
+				}
+			}
+			if (vcs_folder_found) {
+				if (strcmp(command, "status")) {
+					printf("Displaying status\n");
+					// Something to check if the files have changed
+					while ((entry = readdir(dir)) != NULL) {
+						if (entry -> d_type == DT_DIR && !strcmp(entry->d_name, ".vcs") == 0) {
+
+						}
+					}
+				} else if(strcmp(command, "log")) {
+					printf("Displaying log\n");
+					char log_file_path[PATH_MAX];
+					snprintf(log_file_path, PATH_MAX, "%s/%s", absolute_root_path, "log");
+					FILE *log_file_ptr;
+					log_file_ptr = fopen(log_file_path, "r");
+					if (log_file_ptr == NULL) {
+						printf("Nothing has been logged!");
+						return 0;
+					}
+				} else if(strcmp(command, "add")) {
+					char* what_to_add = argv[3];
+					if (strcmp(what_to_add, ".")) {
+						what_to_add =
+					}
+					if (access()) {
+
+					}
+					printf("Adding something\n");
+				} else if(strcmp(command, "reset")) {
+					printf("Reverting changes\n");
+				}
+			} else {
+				printf("Please, initialize your project first using vcs init!");
+				return 0;
+			}
+			}
 	}
 }
